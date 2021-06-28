17734b1b99a8c43b6714cfb64cdd5f23e4d22161
Shin <sin37minduke@naver.com>
2021-06-29 07:33:15 +0900

    Add transition, side bar scroll

--- Week3/week3과제_유튜브.js
+++ Week3/week3과제_유튜브.js
@@ -1,7 +1,7 @@
 var IsSideMenuBtn = false;
-var closedMenuTags = document.getElementById("closedMenu").innerHTML;
-var openedMenuTags = document.getElementById("openedMenu").innerHTML;
-var sideBar = document.getElementById("sideBar");
+// var closedMenuTags = document.getElementById("closedMenu").innerHTML;
+// var openedMenuTags = document.getElementById("openedMenu").innerHTML;
+var sideBarOpened = document.getElementById("sideBarOpened");
 var videoBody = document.getElementById("videoBody");
 // 보통 DOM에서 태그를 가져와 변수에 저장할 때 네이밍 규칙?
 
@@ -18,12 +18,18 @@ function SideMenuBtn() {
 }
 
 function OpenSideMenuAction() {
-    sideBar.innerHTML = openedMenuTags;
-    sideBar.style.width = "225px";
-    videoBody.style.marginLeft = "225px";
+    videoBody.style.marginLeft = "250px";
+    sideBarOpened.style.left = "0px";
 }
 function CloseSideMenuAction() {
-    sideBar.innerHTML = closedMenuTags;
-    sideBar.style.width = "fit-content";
     videoBody.style.marginLeft = "100px";
+    sideBarOpened.style.left = "-300px";
 }
+
+// function OpenSideMenuAction() {
+//     document.getElementById("openedMenu").style.left = "0px";
+// }
+
+// function CloseSideMenuAction() {
+//     document.getElementById("openedMenu").style.left = "-200px";
+// }
